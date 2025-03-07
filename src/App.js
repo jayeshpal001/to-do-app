041991bf5eaa97b85989fc766dbef089e5160d87
jayeshpal001 <jayashpal5@gmail.com>
2025-03-07 16:15:42 +0530

    add demo2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Try from './component/Try';
 import Demo from './component/Demo';
-
+import { Demo2 } from './component/Demo2';
 function App() {
   return (
     <div>
@@ -10,6 +10,7 @@ function App() {
       <h2>i wanna change in repo </h2>
       <Try/>
       <Demo/>
+      <Demo2/>
     </div>
   );
 }
---
+++ src/component/Demo2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Demo2 = () => {
+  return (
+    <div>
+        <h2>This is another</h2>
+        <p>To push something git repositery</p>
+    </div>
+  )
+}
