f6f793df096ba0f1a857e4817f38176874f12f5a
unknown <wordipanshu07@gmail.com>
2025-03-07 15:30:06 +0530

    something new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Try from './component/Try';
 
 function App() {
   return (
     <div>
       <h1>This is first</h1>
+      <h2>i wanna change in repo </h2>
+      <Try/>
     </div>
   );
 }
---
+++ src/component/Try.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function Try() {
+    return (  
+        <div>jayshree thakre</div>
+    );
+}
+
+export default Try;
