3db7936b98be4e123efd908b8fa8b9707de03eaa
jayeshpal001 <jayashpal5@gmail.com>
2025-03-19 01:28:15 +0530

    add routes in app.js

--- src/App.js
+++ src/App.js
@@ -2,55 +2,67 @@ import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { Todos } from "./components/Todos";
 import { AddTodo } from "./components/AddTodo";
-import { useState } from 'react';
+import { useState } from "react";
+import { About } from "./components/About";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
-  let initTodo; 
-  if (localStorage.getItem("todos")===null) {
-    initTodo=[];
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
   } else {
-    initTodo=JSON.parse(localStorage.getItem("todos"));
+    initTodo = JSON.parse(localStorage.getItem("todos"));
   }
   const onDelete = (todo) => {
-    setTodos(todos.filter((e) => e !== todo));
-    localStorage.setItem("todos",JSON.stringify(todos));
+    setTodos((prevTodos) => {
+      const updatedTodos = prevTodos.filter((e) => e !== todo);
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
+      return updatedTodos;
+    });
   };
-  const addTodo =(title, desc)=>{
+
+  const addTodo = (title, desc) => {
     console.log("I am adding this todo", title, desc);
-    let sno; 
-    if (todos.length===0) {
-      sno=1;
+    let sno;
+    if (todos.length === 0) {
+      sno = 1;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
     }
-    else{
-       sno = todos[todos.length-1].sno+1;
-    }
-   
-    const myTodo={
-      sno: sno, 
-      title: title, 
+
+    const myTodo = {
+      sno: sno,
+      title: title,
       desc: desc,
-    }
+    };
     setTodos([...todos, myTodo]);
     console.log(myTodo);
-  }
+  };
 
   const [todos, setTodos] = useState(initTodo);
-  
+
   useEffect(() => {
-    localStorage.setItem("todos",JSON.stringify(todos));
-  }, [todos])
-  
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <div className="h-screen flex flex-col">
       {/* Navbar at the top */}
-      <Navbar />
-      <AddTodo addTodo={addTodo}/>
-      {/* Todo Section should take full height */}
-      <div className="flex-1 flex justify-center items-center bg-gray-900">
-      
-        <Todos todos={todos} onDelete={onDelete} />
-      </div>
-     <Footer/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/"element={
+              <>
+                <AddTodo addTodo={addTodo} />
+                <div className="flex-1 flex justify-center items-center bg-gray-900">
+                  <Todos todos={todos} onDelete={onDelete} />
+                </div>
+              </>
+            } />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. In vitae nihil vero consectetur! Animi rerum deleniti odio velit ex. Harum, aspernatur inventore? Ex possimus est fugiat velit neque odio unde.</div>
+    )
+}
